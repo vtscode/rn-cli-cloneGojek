1e3d200f1c75108c22e17771765d27f27217cfa5
Toto Dinar Wijaksono <totodinarwijaksono@Totos-MacBook-Air.local>
2019-11-11 21:26:22 +0700

    cloneGojek init slider gofood

--- Coba.js
+++ Coba.js
@@ -117,8 +117,8 @@ const instruction = Platform.select({
                         <View style={{position:'relative'}}>
                             <Image source={require('./dummy/sepak-bola.jpg')} style={{width:'100%', height:170,borderRadius:7}} />
                             
-                            <View style={{width:'100%',height:'100%',position:'absolute',top:0,left:0,backgroundColor:'black',opacity:0.2,borderRadius:7}}></View>
-                            <View style={{width:55, height:15,position:'absolute',top:16,left:16}} >
+                            <View style={{width:'100%',height:'100%',position:'absolute',top:0,left:0,backgroundColor:'black',opacity:0.15,borderRadius:7}}></View>
+                            <View style={{width:60, height:15,position:'absolute',top:16,left:16}} >
                                 <Image source={require('./logo/white.png')} style={{width:undefined,height:undefined,resizeMode:'contain',flex:1}}/>
                             </View> 
                         </View>
@@ -131,6 +131,63 @@ const instruction = Platform.select({
                         </View>
                     </View>
 
+                    {/* Internal information */}
+                    <View style={{padding:17,paddingBottom:0}}>
+                        <View style={{width:60, height:15,marginLeft:-4}} >
+                            <Image source={require('./logo/gojek.png')} style={{width:undefined,height:undefined,resizeMode:'contain',flex:1}}/>
+                        </View>
+                        <Text style={{fontSize:17,fontWeight:'bold',color:'#1c1c1c',marginTop:15,marginBottom:20}}>Complete your profile</Text>
+                        <View style={{flexDirection:'row',marginBottom:16}}>
+                            <View>
+                                <Image source={require('./dummy/facebook-connect.png')} style={{}} />
+                            </View>
+                            <View style={{marginLeft:16,flex:1}}>
+                                <Text style={{fontSize:15,fontWeight:'bold',color:'#4a4a4a'}}>Connect to Facebbok</Text>
+                                <Text style={{fontSize:15,fontWeight:'normal',color:'#4a4a4a',width:'70%'}}>Log in faster without verification code</Text>
+
+                            </View>
+                        </View>
+                        <TouchableOpacity style={{backgroundColor:'#61a756',paddingHorizontal:12,paddingVertical:11,alignSelf:'flex-end',borderRadius:4}}>
+                            <Text style={{fontSize:10,fontWeight:'bold',color:'white'}}>CONNECT</Text>
+                        </TouchableOpacity>
+                        <View style={{borderBottomColor:'#e8e9ed',borderBottomWidth:1,marginTop:16}}></View>
+                    </View>
+
+
+                    {/* Gofood banner section */}
+                    <View style={{padding:16}}>
+                        <View style={{position:'relative'}}>
+                            <Image source={require('./dummy/food-banner.jpg')} style={{width:'100%', height:170,borderRadius:7}} />
+                            <View style={{width:'100%',height:'100%',position:'absolute',top:0,left:0,backgroundColor:'black',opacity:0.15,borderRadius:7}}></View>
+                            <View style={{width:60, height:15,position:'absolute',top:16,left:16}} >
+                                <Image source={require('./logo/white.png')} style={{width:undefined,height:undefined,resizeMode:'contain',flex:1}}/>
+                            </View>
+                            <View style={{position:'absolute',bottom:0,left:0,width:'100%',flexDirection:'row',alignItems:'center',paddingHorizontal:16,paddingBottom:16}}>
+                                <View>
+                                    <Text style={{fontSize:18,fontWeight:'bold',color:'white',marginBottom:8}}>Free Go-Food Voucher</Text>
+                                    <Text style={{fontSize:12,fontWeight:'400',color:'white'}}>Quick, before they run out !</Text>
+                                </View>
+                                <View style={{flex:1,paddingLeft:12}}>
+                                <TouchableOpacity style={{backgroundColor:'#61a756',paddingHorizontal:12,paddingVertical:11,alignSelf:'flex-end',borderRadius:4}}>
+                                    <Text style={{fontSize:10,fontWeight:'bold',color:'white'}}>GET VOUCHER</Text>
+                                </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={{borderBottomColor:'#e8e9ed',borderBottomWidth:1,marginTop:16}}>
+                        </View>
+                    </View>
+
+                    {/* Nearby Go-Food */}
+                    <View>
+                        <View style={{width:60, height:15,marginLeft:-4}} >
+                            <Image source={require('./logo/go-food.png')} style={{width:undefined,height:undefined,resizeMode:'contain',flex:1}}/>
+                        </View>
+                    </View>
+                    <View style={{flexDirection:'row'}}>
+                        <Text>Nearby Restaurant</Text>
+                        <Text>See All</Text>
+                    </View>
 
                 </ScrollView>
                 
